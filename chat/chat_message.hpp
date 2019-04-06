bb97365080aea4c15967f866cc856880385e62e9
Douglas <dtoddzenger@gmail.com>
2019-04-06 15:38:50 -0500

    Added usernames to messages and skeleton on spell check

--- chat/chat_message.hpp
+++ chat/chat_message.hpp
@@ -20,7 +20,16 @@ class chat_message
 public:
   enum { header_length = 4 };
   enum { max_body_length = 512 };
-	std::string user_name;
+  enum { max_username = 24 };
+
+  char username[max_username];//This is the username that is to be used
+
+  //This function sets the username for a user
+  void set_username(char *name){
+    std::memcpy(this->username,name,max_username);
+    username_length=std::strlen(name);
+  }
+
   chat_message()
     : body_length_(0)
   {
@@ -38,17 +47,17 @@ public:
 
   std::size_t length() const
   {
-    return header_length + body_length_;
+    return header_length + body_length_+username_length;
   }
 
   const char* body() const
   {
-    return data_ + header_length;
+    return data_ + header_length+username_length;
   }
 
   char* body()
   {
-    return data_ + header_length;
+    return data_ + header_length +username_length;
   }
 
   std::size_t body_length() const
@@ -68,6 +77,14 @@ public:
     char header[header_length + 1] = "";
     std::strncat(header, data_, header_length);
     body_length_ = std::atoi(header);
+    
+    //Added finding the username that is sent
+    char username_size[5]="";
+    std::strncat(username_size, data_+5, 4);
+    username_length = std::atoi(username_size);
+
+    std::strncat(username,data_+10,username_length);
+
     if (body_length_ > max_body_length)
     {
       body_length_ = 0;
@@ -79,12 +96,15 @@ public:
   void encode_header()
   {
     char header[header_length + 1] = ""; 
-    std::sprintf(header, "%4d", static_cast<int>(body_length_));
-    std::memcpy(data_, header, header_length);
+    std::sprintf(header, "%4d%4d%s", static_cast<int>(body_length_),static_cast<int>(username_length),username);
+    std::memcpy(data_, header, header_length+max_username);
+    
   }
 
 private:
-  char data_[header_length + max_body_length];
+
+  char data_[header_length + max_body_length+max_username];
+  std::size_t username_length;//This is the username length
   std::size_t body_length_;
 };
 
