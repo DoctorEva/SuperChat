de3e5747ffd55ebd5bc72bb0a48d2f0246ddfc1b
Douglas <dtoddzenger@gmail.com>
2019-04-07 13:43:27 -0500

    Updated and added functionality to word search

--- chat/chat_client.cpp
+++ chat/chat_client.cpp
@@ -14,12 +14,57 @@
 #include <thread>
 #include <string.h>
 #include <stdio.h>
+
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+
 #include "asio.hpp"
 #include "chat_message.hpp"
 
 using asio::ip::tcp;
 
 typedef std::deque<chat_message> chat_message_queue;
+
+class word_search{
+  public:
+
+  word_search(std::string filename){
+    //first read file
+    string line;
+    std::ifstream myfile (filename);
+    if (myfile.is_open())
+    {
+      while ( std::getline (myfile,line) )
+      {
+        word_list.push_back(line);
+      }
+      myfile.close();
+    }
+    //sort the list alphebetically
+    sort(word_list.begin(), word_list.end(), sortname);
+    //hopefully then it's ready for a binary search
+  }
+
+  //This function checks the word if it is spelt correctly
+  //true is returned if spelt correctly
+  //else false is returned
+  bool check_word(std::string word_check){
+      //TODO: create a comparison if a word is spelt correctly using binary search
+    if(binary_search(word_list.begin(), word_list.end(),word_check)){
+      return true;
+    }
+    return false;
+      
+  }
+
+  private:
+  std::vector<std::string> word_list;
+
+  bool sortname(const std::string &lhs, const std::string &rhs) { return *lhs < *rhs; }
+}
+
 
 class chat_client
 {
